366a39806dd1bddbace5c49aa880bd59558aecf6
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-02-24 17:30:11 +0700

    Add page logic to workexperience.gohtml

--- internal/pkg/workexperience/models.go
+++ internal/pkg/workexperience/models.go
@@ -7,7 +7,7 @@ type WorkExperienceDB struct {
 	StartYear       string   `bson:"start_year"`
 	EndMonth        string   `bson:"end_month"`
 	EndYear         string   `bson:"end_year"`
-	IsCurrentWork   string   `bson:"is_current_work"`
+	IsCurrentWork   bool     `bson:"is_current_work"`
 	Position        string   `bson:"position"`
 	Description     []string `bson:"description"`
 }
