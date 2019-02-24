a6b6c61bc2d1c9fb8d8cc805f4e4800f4e731f0a
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2019-02-24 17:13:27 +0700

    Edit WorkExperienceDB struct

--- internal/pkg/workexperience/models.go
+++ internal/pkg/workexperience/models.go
@@ -1,12 +1,13 @@
 package workexperience
 
 type WorkExperienceDB struct {
-	Company       string   `bson:"company"`
-	StartMonth    string   `bson:"start_month"`
-	StartYear     string   `bson:"start_year"`
-	EndMonth      string   `bson:"end_month"`
-	EndYear       string   `bson:"end_year"`
-	IsCurrentWork string   `bson:"is_current_work"`
-	Position      string   `bson:"position"`
-	Description   []string `bson:"description"`
+	CompanyName     string   `bson:"company_name"`
+	CompanyLocation string   `bson:"company_location"`
+	StartMonth      string   `bson:"start_month"`
+	StartYear       string   `bson:"start_year"`
+	EndMonth        string   `bson:"end_month"`
+	EndYear         string   `bson:"end_year"`
+	IsCurrentWork   string   `bson:"is_current_work"`
+	Position        string   `bson:"position"`
+	Description     []string `bson:"description"`
 }
